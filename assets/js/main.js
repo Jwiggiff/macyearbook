2f95a5726af3df1c7090078eb2ee12db21794ecb
Jwiggiff <friedman.josh03@gmail.com>
2021-01-21 09:57:10 -0500

    Add clubs and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,26 @@
-console.log(document.querySelectorAll(".trigger > li"));
 document.querySelectorAll(".trigger > li").forEach((li) => {
-  li.addEventListener("click", (e) => {
+  li.querySelector("a").addEventListener("click", (e) => {
     li.classList.toggle("active");
     document.querySelectorAll(".trigger > li").forEach((sibling) => {
       if (sibling != li) sibling.classList.remove("active");
     });
   });
 });
-
 window.addEventListener("click", (e) => {
-  if (!e.target.classList.contains("page-link"))
+  if (e.target.closest(".trigger") == null) {
     document
       .querySelectorAll(".trigger > li")
       .forEach((li) => li.classList.remove("active"));
+  }
+});
+
+document.querySelectorAll(".alpha-links > a").forEach((a) => {
+  const target = document.getElementById(a.getAttribute("href")[1]);
+  if (target == null) a.removeAttribute('href');
+  else
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+      a.parentNode.parentNode.scrollTop =
+        target.offsetTop - target.offsetHeight * 1.5;
+    });
 });
