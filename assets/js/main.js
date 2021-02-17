9165a1835e95dd83547b435c0d98bc2c4c5b29d2
Jwiggiff <friedman.josh03@gmail.com>
2021-02-17 16:48:34 -0500

    Add alpha links

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,12 +15,28 @@ window.addEventListener("click", (e) => {
 });
 
 document.querySelectorAll(".alpha-links > a").forEach((a) => {
-  const target = document.getElementById(a.getAttribute("href")[1]);
+  const target = document.getElementById(a.getAttribute("href").slice(1));
   if (target == null) a.removeAttribute('href');
-  else
+  if (a.parentNode.parentNode.classList.contains('dropdown'))
     a.addEventListener("click", (e) => {
       e.preventDefault();
       a.parentNode.parentNode.scrollTop =
         target.offsetTop - target.offsetHeight * 1.5;
     });
 });
+
+const names = [
+  "Josh Friedman",
+  "Jesus Christ",
+  "Our Lord and Saviour",
+  "Stud Move",
+  "Santa Suit"
+];
+let currentIndex = 0;
+document.querySelector('.site-footer p:last-child span')
+  .addEventListener('wheel', (e) => {
+    e.preventDefault();
+    currentIndex += e.deltaY < 0 ? -1 : 1;
+    currentIndex = Math.max(0, Math.min(names.length-1, currentIndex));
+    e.target.innerText = names[currentIndex];
+  });
